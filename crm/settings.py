7ff359f79e4b0f4b0a277c197460756ba193f77a
MD Shoaib Ali <shoaib.ali@example.com>
2024-11-05 10:44:06 +0530

    Updated ALLOWED_HOSTS

--- crm/settings.py
+++ crm/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = 'django-insecure-&1u1@wpg1y8rro8zmgi@%1fg%ju@fu$q^*%3tum&eds6a(!4zf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.108.252.221']
+ALLOWED_HOSTS = ['127.0.0.1', '3.108.252.221']
+
 
 
 
